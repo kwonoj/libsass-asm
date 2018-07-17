c20383f35f7c5da93b792a2e21b2fa9b6fac69bc
OJ Kwon <kwon.ohjoong@gmail.com>
2018-07-16 21:46:03 -0700

    refactor(cwrap): list out sass interfaces
    - relates with #34

--- src/interop/importer/wrapSassImporter.ts
+++ src/interop/importer/wrapSassImporter.ts
@@ -7,6 +7,7 @@ import { cwrapSignature } from 'emscripten-wasm-loader';
  */
 const wrapSassImporter = (_cwrap: cwrapSignature) => ({
   //Sass_C_Import_Callback sass_make_importer (Sass_C_Import_Fn, void* cookie);
+  make_importer: null,
   //Sass_C_Import_Fn sass_import_get_function (Sass_C_Import_Callback fn);
   import_get_function: () => {
     throw new Error('not implemented');
@@ -14,25 +15,43 @@ const wrapSassImporter = (_cwrap: cwrapSignature) => ({
   //void* sass_import_get_cookie (Sass_C_Import_Callback fn);
   import_get_cookie: () => {
     throw new Error('not implemented');
-  }
+  },
   //void sass_delete_importer (Sass_C_Import_Callback fn);
+  delete_importer: null,
   //Sass_Import_Entry* sass_make_import_list (size_t length);
+  make_import_list: null,
   //Sass_Import_Entry sass_make_import_entry (const char* path, char* source, char* srcmap);
+  make_import_entry: null,
   //Sass_Import_Entry sass_make_import (const char* rel, const char* abs, char* source, char* srcmap);
+  make_import: null,
   //Sass_Import_Entry sass_import_set_error(Sass_Import_Entry import, const char* message, size_t line, size_t col);
+  import_set_error: null,
   //void sass_import_set_list_entry (Sass_Import_Entry* list, size_t idx, Sass_Import_Entry entry);
+  import_set_list_entry: null,
   //Sass_Import_Entry sass_import_get_list_entry (Sass_Import_Entry* list, size_t idx);
+  import_get_list_entry: null,
   //const char* sass_import_get_imp_path (Sass_Import_Entry);
+  import_get_imp_path: null,
   //const char* sass_import_get_abs_path (Sass_Import_Entry);
+  import_get_abs_path: null,
   //const char* sass_import_get_source (Sass_Import_Entry);
+  import_get_source: null,
   //const char* sass_import_get_srcmap (Sass_Import_Entry);
+  import_get_srcmap: null,
   //char* sass_import_take_source (Sass_Import_Entry);
+  import_take_source: null,
   //char* sass_import_take_srcmap (Sass_Import_Entry);
+  import_take_srcmap: null,
   //size_t sass_import_get_error_line (Sass_Import_Entry);
+  import_get_error_line: null,
   //size_t sass_import_get_error_column (Sass_Import_Entry);
+  import_get_error_column: null,
   //const char* sass_import_get_error_message (Sass_Import_Entry);
+  import_get_error_message: null,
   //void sass_delete_import_list (Sass_Import_Entry*);
+  delete_import_list: null,
   //void sass_delete_import (Sass_Import_Entry);
+  delete_import: null
 });
 
 export { wrapSassImporter };
