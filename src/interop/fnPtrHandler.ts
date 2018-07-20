356bf24c5a046b4396303db4fdd513edb12599b5
OJ Kwon <kwon.ohjoong@gmail.com>
2018-07-20 14:54:03 -0700

    feat(getfnptrhandler): wrapper to add, removefunction

---
+++ src/interop/fnPtrHandler.ts
@@ -0,0 +1,26 @@
+import { SassAsmModule } from '../SassAsmModule';
+
+/**
+ * Returns handler object to wrap `addFunction` and `removeFunction`
+ */
+const getFnPtrHandler = ({ addFunction, removeFunction }: SassAsmModule) => {
+  let count = 0;
+
+  return {
+    add: (fn: Function) => {
+      if (count === 20) {
+        throw new Error('Maximum number of function pointer added');
+      }
+      count++;
+      return addFunction(fn);
+    },
+    remove: (fnPtr: number) => {
+      if (count > 0) {
+        count--;
+        removeFunction(fnPtr);
+      }
+    }
+  };
+};
+
+export { getFnPtrHandler };
