e1ddbe5cdf5ac69a41e1895fb6158f8f8dfb0454
OJ Kwon <kwon.ohjoong@gmail.com>
2018-08-04 17:40:48 -0700

    fix(addfunction): correct addfunction signature

--- src/interop/fnPtrHandler.ts
+++ src/interop/fnPtrHandler.ts
@@ -7,12 +7,12 @@ const getFnPtrHandler = ({ addFunction, removeFunction }: SassAsmModule) => {
   let count = 0;
 
   return {
-    add: (fn: Function) => {
+    add: (fn: Function, signature: string) => {
       if (count === 20) {
         throw new Error('Maximum number of function pointer added');
       }
       count++;
-      return addFunction(fn);
+      return addFunction(fn, signature);
     },
     remove: (fnPtr: number) => {
       if (count > 0) {
